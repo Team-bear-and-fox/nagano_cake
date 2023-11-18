195276134ca4409c9f7a535790115036e18ae6bc
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-11-18 04:50:28 +0000

    add view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to:"homes#top"
+  root to:"public/homes#top"
   resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 
   resources :orders, only: [:new, :create, :index, :show]do
