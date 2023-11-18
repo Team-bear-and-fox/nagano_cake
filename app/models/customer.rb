70067d5c0fde61e333db97ef37362ed0778395f5
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-11-18 15:54:40 +0000

    Add View header,log_in,sign_up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,12 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :postal_code, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
 end
