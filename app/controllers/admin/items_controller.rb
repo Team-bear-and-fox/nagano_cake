23a44aa40f2923ddfbcb901866235e8464742938
WakasugiSatoi <wakasugi.sa101@gmail.com>
2023-11-19 01:57:36 +0000

    change admin item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,28 @@
 class Admin::ItemsController < ApplicationController
+  
+  def new
+    @item = Item.new
+    @genre = Genre.pluck(:genre_id)
+    @genre = 
+  end 
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path
+  end
+  
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :value, :genre_id, :is_on_sale)
+  end
+  
 end
