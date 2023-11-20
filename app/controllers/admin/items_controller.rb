c6ef1b2392fbd7838ff4ecfda78b7a8c5dea3575
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-11-20 02:23:43 +0000

    tax change

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,17 +18,17 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def edit
     @item = Item.find(params[:id])
   end
-  
+
   def update
     item = Item.find(params[:id])
     item.update(item_params)
     redirect_to admin_item_path(item.id)
   end
-  
+
   def get_image
     image
   end
