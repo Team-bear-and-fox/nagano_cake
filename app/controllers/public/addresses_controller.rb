d505541ffe10a9a2cb23dc5ea2ebd4aa68c4d81c
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-11-20 04:16:36 +0000

    addresses index create

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,18 @@
 class Public::AddressesController < ApplicationController
-  def new
+  def index
+    @addresses = Address.all
     @address = Address.new
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
   end
 end
