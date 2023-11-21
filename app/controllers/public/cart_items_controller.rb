8f46e1f8cfacf0667ddd5670610aac5432089b47
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-11-21 09:20:34 +0000

    Fix View cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,23 +7,31 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @item = Item.find(cart_item_params[:item_id])
-    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = current_customer.cart_items.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.item_id = @item.id
-    @cart_item = current_customer.cart_items.build(cart_item_params)
     @cart_items = current_customer.cart_items.all
     @cart_items.each do |cart_item|
-    if cart_item.item_id == @cart_item.item_id
-      new_amount = cart_item.amount + @cart_item.amount
-      cart_item.update_attribute(:amount, new_amount)
-      @cart_item.delete
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+      end
+    end
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      @item = Item.find(cart_item_params[:item_id])
+      redirect_to item_path(@item.id)
     end
   end
-    @cart_item.save
-    redirect_to cart_items_path
-  end
   def update
-    
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path(@cart_item.id)
+    else
+      render :index
+    end
   end
   def destroy
     @cart_item = CartItem.find(params[:id])
