8691abf8a15c338bc9583866ff9a098c7fedd366
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-11-21 05:36:06 +0000

    complete

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -7,8 +7,12 @@ class Public::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-    @address.save
-    redirect_to request.referer
+    if @address.save
+      redirect_to request.referer
+    else
+       @addresses = Address.all
+      render :index
+    end
   end
 
   def edit
@@ -16,9 +20,18 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+     render :edit
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to request.referer
   end
 
   private
