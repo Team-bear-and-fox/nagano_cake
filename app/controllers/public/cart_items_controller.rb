a9a3c257cd797d8844ec32f971a27f1371bfd696
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-11-21 05:36:49 +0000

    Add View cart_item index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,38 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   def index
-    # @book_comment = current_user.book_comments.find_by(book_id: @item.id)
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.add_sub_total}
   end
 
   def create
-    
     @item = Item.find(cart_item_params[:item_id])
     @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
     @cart_item.item_id = @item.id
-    binding.pry
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+    if cart_item.item_id == @cart_item.item_id
+      new_amount = cart_item.amount + @cart_item.amount
+      cart_item.update_attribute(:amount, new_amount)
+      @cart_item.delete
+    end
+  end
     @cart_item.save
+    redirect_to cart_items_path
+  end
+  def update
+    
+  end
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+  def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
     redirect_to cart_items_path
   end
   private
