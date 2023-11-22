4b637c1d3c30edd03d66183e4ab1305bf61a54d7
mh53182 <marie.i.077145@gmail.com>
2023-11-22 07:22:58 +0000

    Fix Source

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -46,70 +46,8 @@ class Public::OrdersController < ApplicationController
       @order_detail.value = cart_item.item.add_tax_value
       @order_detail.save
     end
-      current_customer.cart_items.destroy_all
-      redirect_to complete_orders_path
-    # else
-    #   flash.now[:notice] = ""
-
-
-    # @order = Order.new
-    # @order.customer_id = current_customer.id
-    # @order.postage = 800
-    # @cart_items = CartItem.where(customer_id: current_customer.id)
-
-    # prices = []
-    # @cart_items.each do |cart_item|
-    #   prices << cart_item.item.value*cart_item.amount
-    # end
-
-    # @cart_items_total = prices.sum
-    # @order.payment_total = @order.postage + @cart_items_total
-    # @order.payment_method = params[:order][:payment_method]
-    # if @order.payment_method == "credit_card"
-    #   @order.status = 1
-    # else
-    #   @order.status = 0
-    # end
-
-    # address_type = params[:order][:address_type]
-    # case address_type
-    # when "customer_address"
-    #   @order.postal_code = current_customer.postal_code
-    #   @order.address = current_customer.address
-    #   @order.name = current_customer.last_name + current_customer.first_name
-
-    # when "registered_address"
-    #   Address.find(params[:order][:regestered_address_id])
-    #   # ↑この行要る？
-    #   selected = Address.find(params[:order][:registered_address_id])
-    #   @order.postal_code = selected.postal_code
-    #   @order.address = selected.address
-    #   @order.name = selected.name
-
-    # when "create_new_address"
-    #   @order.postal_code = params[:order][:new_postal_code]
-    #   @order.address = params[:order][:new_address]
-    #   @order.name = params[:order][:new_name]
-    # end
-
-    # if @order.save
-
-    #   if @order.status == 0
-    #     @cart_items.each do |cart_item|
-    #       OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, value: cart_item.item.value, amount: cart_item.amount, production_status: 0)
-    #     end
-    #   else
-    #     @cart_items.each do |cart_item|
-    #       OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, value: cart_item.item.value, amount: cart_item.amount, production_status: 1)
-    #     end
-    #   end
-
-    #   @cart_items.destroy_all
-    #   redirect_to complete_orders_path
-
-    # else
-    #   render :items
-    # end
+    current_customer.cart_items.destroy_all
+    redirect_to complete_orders_path
   end
 
   def complete
@@ -128,8 +66,6 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:customer_id, :postage, :payment_method, :payment_total, :order_status, :name, :postal_code, :address)
-
   end
-
 
 end
