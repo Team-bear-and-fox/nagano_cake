257597ed6ea9c2944e790ba6f2fb78b3b95e9771
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-11-23 14:43:17 +0000

    Add View admin-top,order_index,order_show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,17 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    # @order_details = OrderDetail.where(order_id: @order.id)
+    @customer = @order.customer
+    @order_details = @order.order_details
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    else
+      render :show
+    end
   end
 
   private
