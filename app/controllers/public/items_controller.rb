1ff88fb8d0d489036ae006157fcf672a4050c8e8
WakasugiSatoi <wakasugi.sa101@gmail.com>
2023-11-27 06:18:19 +0000

    fix

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,15 +1,11 @@
 class Public::ItemsController < ApplicationController
- def index
-  @items = Item.all
-  @items_sale = @items.select do |item|
-    item.is_on_sale == true
+  def index
+    @items_sale = Item.where(is_on_sale: true).page(params[:page]).per(8)
+    @genres = Genre.all
   end
-  @item = Item.page(params[:page]).per(8)
-  @genres = Genre.all
- end
 
- def show
-  @item = Item.find(params[:id])
-  @genres = Genre.all
- end
+  def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
 end
