630d6adac6ebd9a57bfdb278c320a2eed7d1f771
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-11-27 03:57:59 +0000

    complete3

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,15 @@
 class Public::ItemsController < ApplicationController
-  def index
-    @items = Item.all
-    @item = Item.page(params[:page]).per(8)
-    @genres = Genre.all
+ def index
+  @item = Item.all
+  @items = Item.all.page(params[:page]).per(8)
+  @items_sale = @items.select do |item|
+    item.is_on_sale == true
   end
+  @genres = Genre.all
+ end
 
-  def show
-    @item = Item.find(params[:id])
-    @genres = Genre.all
-  end
+ def show
+  @item = Item.find(params[:id])
+  @genres = Genre.all
+ end
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,7 +3,9 @@ class Public::SearchesController < ApplicationController
     @genre_id = params[:genre_id]
     @genres = Genre.all
     @genre_name =Genre.find(params[:genre_id])
-    @item = Item.where(genre_id: @genre_id).page(params[:page]).per(8)
-    @items = Item.all
+    @items = Item.where(genre_id: @genre_id).page(params[:page]).per(8)
+    @items_sale = @items.select do |item|
+    item.is_on_sale == true
+    end
   end
 end
